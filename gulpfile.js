20909115e664f351af49659a5d6b2451392d0de0
front-end-jeko <43757639+front-end-jeko@users.noreply.github.com>
2019-03-10 21:09:25 +0400

    update main page

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('scss/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 10 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('css'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('scss/**/*.scss', gulp.parallel('sass'));
+});
---
+++ js/main.js
@@ -0,0 +1,122 @@
+// open all popup by one code
+var openPopup = document.querySelectorAll('.open__popup');
+openPopup.forEach(function(click){
+    click.addEventListener('click', function(e){
+        document.querySelectorAll('.loopex__popup-close').forEach(function(mypopup){
+            if(click.getAttribute('data-popup') ===  mypopup.getAttribute('data-popup')){
+                mypopup.classList.toggle('active');
+            }
+        });
+
+        e.stopPropagation();
+        e.preventDefault();
+    });
+});
+
+
+// stoppropagation stop
+$('.stopPropagation').click(function(e){
+    e.stopPropagation();
+});
+
+
+// close popups by x icon
+var closePopup = document.querySelectorAll('.popup__close');
+closePopup.forEach(function(close){
+    close.addEventListener('click', function(){
+        this.parentElement.parentElement.classList.remove('active');
+    });
+});
+
+
+// close popups by document click
+document.addEventListener('click', function(){
+    document.querySelectorAll('.loopex__popup-close').forEach(function(pop){
+        if(pop.classList.contains('active')){
+            pop.classList.remove('active');
+        }
+    });
+});
+
+
+
+
+
+// slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
+// tabs
+$(document).ready(function () {
+    $('.marketplace__body').hide();
+    
+    $('.marketplace__body:first').show();
+    
+    $('.marketplace__li:first').addClass('active');
+    
+    $('.marketplace__li').click(function () {
+    
+        $('.marketplace__li').removeClass('active');
+    
+        $(this).addClass('active');
+    
+        var currentTab = $(this).attr('href');
+    
+        $('.marketplace__body').hide();
+    
+        $(currentTab).show();
+    
+        return false;
+    });
+    });
+
+
+
+    // password eye visibility
+    var eyeIcon = document.querySelectorAll('.password__eye');
+
+    eyeIcon.forEach(function(eye){
+        var parent = eye.parentElement;
+        var parntChild = parent.querySelector('.loopex__input');
+
+        var attr = parntChild.getAttribute('type');
+
+        if(parntChild.attr == 'password'){
+            eye.style.color = 'green';
+        }
+
+        console.log(attr);
+    });
+
+    // console.log(eyeIcon);
+
+    
