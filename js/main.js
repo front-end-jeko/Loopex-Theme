578ba23f8a49ba17ee512778c943e7611b9cf563
front-end-jeko <jemali.tediashvili10b@geolab.edu.ge>
2019-07-04 19:15:27 +0400

    update theme

--- js/main.js
+++ js/main.js
@@ -180,7 +180,7 @@ eyeIcon.forEach(function(eye){
 
 // time
 // Set the date we're counting down to
-var countDownDate = new Date("jun 5, 2019 15:37:25").getTime();
+var countDownDate = new Date("oct 5, 2019 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
